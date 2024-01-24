fa815bfc2c68f88973e3aa0a8b40ca0bc3f0a28b
PyrinAndrii <pyrin.andrii@gmail.com>
2024-01-24 19:20:44 +0100

    Add CONSTRAINTS examples to tables' creation

--- create_tables.sql
+++ create_tables.sql
@@ -18,20 +18,30 @@ CREATE TABLE units (
 );
 
 CREATE TABLE working_places (
-	id serial PRIMARY KEY,
+	id serial,
   floor INT NOT NULL,
-	room INT NOT NULL,
-	place_number INT NOT NULL
+	room INT NOT NULL CHECK ( room > 0 ),
+	place_number INT NOT NULL,
+	PRIMARY KEY ( id ),
+	-- CONSTRAINTS
+	CONSTRAINT valid_floor CHECK ( floor >= 0 AND floor <= 501 ),
+	CONSTRAINT unique_place UNIQUE ( floor, room, place_number )
 );
 
 CREATE TABLE working_places_equipments (
 	id serial PRIMARY KEY,
 	working_place_id INT NOT NULL,
+	-- working_place_id INT NOT NULL REFERENCES working_places ( id ), -- alternative
+	-- working_place_id INT NOT NULL REFERENCES working_places, -- alternative
 	desk boolean DEFAULT FALSE NOT NULL,
 	chair boolean DEFAULT FALSE NOT NULL,
 	stationery_items boolean DEFAULT FALSE NOT NULL,
 	FOREIGN KEY ( working_place_id )
-		REFERENCES working_places (id )
+		REFERENCES working_places ( id )
+		-- ON DELETE NO ACTION -- default
+		-- ON DELETE SET NULL -- alternative
+		-- ON DELETE SET DEFAULT -- alternative, if default value is set
+		-- ON UPDATE CASCADE -- alternative
 		ON DELETE CASCADE
 );
 
