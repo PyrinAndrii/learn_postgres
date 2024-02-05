3bc83992ecb264e0be13d26b60ca735d63b0ee01
PyrinAndrii <pyrin.andrii@gmail.com>
2024-02-05 20:34:12 +0100

    Add alter table commands samples

---
+++ alter_tables.sql
@@ -0,0 +1,25 @@
+ALTER TABLE working_places
+  ADD COLUMN chairs integer NOT NULL CHECK( chairs <= 2 );
+
+ALTER TABLE working_places ADD COLUMN chairs integer;
+ALTER TABLE working_places ALTER COLUMN chairs SET NOT NULL;
+ALTER TABLE working_places ADD CHECK( chairs <= 2 );
+
+ALTER TABLE working_places ALTER COLUMN chairs DROP NOT NULL;
+ALTER TABLE working_places DROP CONSTRAINT working_places_chairs_check;
+ALTER TABLE working_places DROP COLUMN chairs;
+
+
+ALTER TABLE task_goals
+  ADD COLUMN cost float NOT NULL DEFAULT 0;
+
+ALTER TABLE task_goals
+  ALTER COLUMN cost SET DATA TYPE numeric( 5,2 );
+
+ALTER TABLE working_places DROP CONSTRAINT "unique_place";
+ALTER TABLE working_places ADD CONSTRAINT unique_place UNIQUE (floor, room, place_number);
+ALTER TABLE working_places ADD UNIQUE (floor, room, place_number); -- generates working_places_floor_room_place_number_key
+
+ALTER TABLE workers
+  RENAME CONSTRAINT workers_second_working_place_id_fkey
+  TO second_working_place_id_for_workers_fkey;
