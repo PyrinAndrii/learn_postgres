ea00b1f2520ff35ff442a484141ba08f90292662
PyrinAndrii <pyrin.andrii@gmail.com>
2024-02-16 18:25:56 +0100

    Add materialized view

--- views.sql
+++ views.sql
@@ -13,3 +13,17 @@ CREATE OR REPLACE VIEW similar_task_goals
 
 SELECT * FROM similar_task_goals;
 DROP VIEW IF EXISTS similar_task_goals;
+
+
+CREATE MATERIALIZED VIEW materialized_similar_task_goals
+  AS
+    SELECT task_id,
+          title,
+          count( * ) AS task_goals_count,
+          SUM(cost) AS total_cost
+      FROM task_goals
+      GROUP BY task_id, title
+      ORDER BY task_goals_count DESC, total_cost DESC; -- WITH NO DATA;
+
+REFRESH MATERIALIZED VIEW materialized_similar_task_goals;
+DROP MATERIALIZED VIEW materialized_similar_task_goals;
